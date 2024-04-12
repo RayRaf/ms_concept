4bac1ed61f2c8df1a8e224297cbf9f1c3cb165fc
RayRaf <rayraf@mail.ru>
2024-04-12 15:06:46 +0300

    Allow all hosts

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)8+u!p!l#4_7&px=1+^!470&+76dp(p=ys6sshp1!88os5h=@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['* ']
 
 
 # Application definition
